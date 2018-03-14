304aafb64398359ac967e929fc60883b03ccd709
Kevin Ring <kevin@kotachrome.com>
2018-03-15 10:32:45 +1100

    gzip NM catalog.

--- publish.sh
+++ publish.sh
@@ -9,4 +9,6 @@ then
 fi
 
 # Publish the catalog to an S3 bucket on AWS.
-aws s3 --profile nationalmap cp build/nm.json s3://static.nationalmap.nicta.com.au/init/$1.json
+gzip --keep build/nm.json
+aws s3 --profile nationalmap cp --content-encoding gzip --content-type "application/json" build/nm.json.gz s3://static.nationalmap.nicta.com.au/init/$1.json
+rm build/nm.json.gz
